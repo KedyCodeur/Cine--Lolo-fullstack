c9be8caf551ec90a15c82ce63e2a39cc09900b57
Cem <cemsahozdemirel791@gmail.com>
2026-03-30 11:58:45 +0200

    login/register - Sans php js

---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Connexion | Ciné Lolo</title>
+        
+        <meta name="author" content="KedyCodeur">
+        <meta name="description" content="Page de connexion sécurisée pour accéder à votre espace Ciné Lolo.">
+        <link rel="stylesheet" href="css/login_register.css">
+        <link rel="icon" type="image/png" href="./assets/icon.png">
+       <style>
+            @import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+       </style>
+    </head>
+   
+    <body class="loginBackground">
+
+        <div class="container">
+
+            <h1 class="loginTitle">Connectez-vous</h1>
+            
+            <div class="inputContainer">
+                <div class="inputFlexColumn">
+                    <label for="mail" class="labelLogin">Mail</label>
+                    <input type="text" name="mail" class="inputLogin">
+                </div>
+
+                <div class="inputFlexColumn">
+                    <label for="password" class="labelLogin">Mot de passe</label>
+                    <input type="password" name="password" class="inputLogin">
+                    <label for="rememberMe" class="rememberMe">Se souvenir de moi<input type="checkbox" name="rememberMe" id="rememberMe"></label>
+                    
+                </div>
+                
+            </div>
+
+            
+            <button class="loginButton" >Se Connecter</button>
+            <p class="redirectionP">Vous n'avez pas de compte ? <a href="./register.php" class="redirectionLink">Inscrivez-vous</a></p>
+
+        </div>
+        
+        
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Inscription | Ciné Lolo</title>
+        
+        <meta name="author" content="KedyCodeur">
+        <meta name="description" content="Page de connexion sécurisée pour accéder à votre espace Ciné Lolo.">
+        <link rel="stylesheet" href="css/login_register.css">
+        <link rel="icon" type="image/png" href="./assets/icon.png">
+       <style>
+            @import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+       </style>
+    </head>
+   
+    <body class="loginBackground">
+
+        <div class="container">
+
+            <h1 class="loginTitle">Inscrivez-vous</h1>
+            
+            <div class="inputContainer">
+                <div class="inputFlexColumn">
+                    <label for="mail" class="labelLogin">Mail</label>
+                    <input type="text" name="mail" class="inputLogin">
+                </div>
+
+                <div class="inputFlexColumn">
+                    <label for="password" class="labelLogin">Mot de passe</label>
+                    <input type="password" name="password" class="inputLogin">                    
+                </div>
+
+                <div class="inputFlexColumn">
+                    <label for="confirmPassword" class="labelLogin">Confirmez le mot de passe</label>
+                    <input type="password" name="confirmPassword" class="inputLogin">                    
+                </div>
+            </div>
+
+            
+            <button class="loginButton" >S'inscrire</button>
+            <p class="redirectionP">Déjà un compte ? <a href="./login.php" class="redirectionLink">Connectez-vous</a></p>
+
+        </div>
+        
+        
+    </body>
+</html>
